95e1b1719df496b5d440f2ac8d88858d39165970
Antonio Vivace <puma95.95@gmail.com>
2017-04-01 13:42:06 +0200

    Webhook listener in Node.js

--- webhook-listener/index.js
+++ webhook-listener/index.js
@@ -4,13 +4,13 @@ var app = express();
 var sqlite3 = require('sqlite3').verbose();
 var db = new sqlite3.Database('/var/www/html/apps/radiogit/db.sqlite');
 
+
 app.get('/', function (req, res) {
     res.send('RadioGit backend is up and running')
 })
 
 app.use(bodyParser.json());
 app.post('/test', function(request, response){
-  //console.log(request.body);
   var text = request.body.repository.full_name + " - " + request.body.commits[0].author.username + " committed " + request.body.commits[0].message + " " + " " + request.body.commits[0].url;
   var query = 'INSERT INTO Payloads VALUES (NULL, "' + request.body.repository.full_name + '","' + text + '")';
   response.send("Thank you kind sir");
@@ -24,6 +24,7 @@ app.post('/test', function(request, response){
 
 app.listen(3000);
 
+// On CTRL+C correctly close the database
 process.on('SIGINT', function(){
   console.log('Cya');
   db.close();
